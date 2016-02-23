9aab2c446d86b7b3e6b5196e21144487b5b41ff6
Tahsin Kocaman <lordtrai@yahoo.com>
2016-02-23 19:29:25 +0200

    Code Readability Improvement

--- ProjectEuler/10-Summation_of_primes.py
+++ ProjectEuler/10-Summation_of_primes.py
@@ -2,13 +2,13 @@
 Find the sum of all the primes below two million.
 '''
 
-def rwh_primes(n):
+def sieve_primes(n):
     """ Returns  a list of primes < n """
     sieve = [True] * n
     for i in range(3,int(n**0.5)+1,2):
         if sieve[i]:
-            sieve[i*i::i]=[False]*((n-i*i-1)//i+1)
+            sieve[i*i::i]=[False]*len(sieve[i*i::i])
     return [2] + [i for i in range(3,n,2) if sieve[i]]
 
-print(sum(rwh_primes(2000000)))
+print(sum(sieve_primes(2000000)))
 
