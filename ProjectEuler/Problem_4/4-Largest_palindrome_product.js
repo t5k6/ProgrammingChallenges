9e243562145866286af8b66787bf04be4cc40f02
t5k6 <lordtrai@yahoo.com>
2018-05-17 23:25:32 +0300

    cosmetic change

--- ProjectEuler/Problem_4/4-Largest_palindrome_product.js
+++ ProjectEuler/Problem_4/4-Largest_palindrome_product.js
@@ -1,10 +1,8 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-let largest = 0
+const palindrome = text => text == [...text].reverse().join('')
 
-const palindrome = text => {
-    return text == [...text].reverse().join('')
-}
+let largest = 0
 
 for (let i = 999; i > 900; i--) {
     for (let j = 999; j > 900; j--) {
