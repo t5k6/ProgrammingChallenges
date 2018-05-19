f2d0bec05ca9758d2da065d9e0595a77888d9807
t5k6 <lordtrai@yahoo.com>
2018-05-19 18:29:29 +0300

    filtering

--- Codewars/7_kyu/codewars_7.rb
+++ Codewars/7_kyu/codewars_7.rb
@@ -1,4 +1,9 @@
 # https://www.codewars.com/kata/list-filtering/train/ruby
 def filter_list(l)
-    l.select { |i| i*0==0 }.compact
-  end
+  l.select { |i| i*0==0 }.compact
+end
+
+# http://www.codewars.com/kata/friend-or-foe/train/ruby
+def friend(friends)
+  friends.select { |name| name.length == 4 }
+end
